e92c0312caa170dd8e31fce7b57c4c1a577bbe92
paulyswen <paul51305040@gmail.com>
2025-09-30 21:58:08 +0800

    implement texteditingcontroller

--- lib/pages/home_screen.dart
+++ lib/pages/home_screen.dart
@@ -10,19 +10,54 @@ class HomeScreen extends ConsumerStatefulWidget {
 }
 
 class _HomeScreenState extends ConsumerState<HomeScreen> {
+  late TextEditingController searchController;
+  @override
+  void initState() {
+    super.initState();
+    searchController = TextEditingController();
+  }
+
+  @override
+  void dispose() {
+    searchController.dispose();
+    super.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     final locationList = ref.watch(fetchLocationListProvider);
     return Scaffold(
       appBar: AppBar(title: const Text('Home')),
       body: locationList.when(
-        data: (data) => ListView.builder(
-          itemCount: data.length,
-          itemBuilder: (context, index) =>
-              ListTile(title: Text(data[index].locationName)),
+        data: (data) => Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: searchController,
+                decoration: InputDecoration(
+                  prefixIcon: const Icon(Icons.search),
+                  border: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(8),
+                  ),
+                  labelText: '搜尋',
+                  hintText: '輸入城市名',
+                ),
+              ),
+              const SizedBox(height: 8),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: data.length,
+                  itemBuilder: (context, index) =>
+                      ListTile(title: Text(data[index].locationName)),
+                ),
+              ),
+            ],
+          ),
         ),
         error: (error, stackTrace) => Text(error.toString()),
-        loading: () => const CircularProgressIndicator(),
+        loading: () =>
+            Center(child: const CircularProgressIndicator.adaptive()),
       ),
     );
   }
